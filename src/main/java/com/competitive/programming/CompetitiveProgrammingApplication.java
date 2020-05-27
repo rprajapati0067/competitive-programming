94c649ac140ef13bcb2bea7ba014670e27358acb
ravi <rprajapati067@gmail.com>
2020-05-27 21:57:54 +0530

    initial commit

---
+++ src/main/java/com/competitive/programming/CompetitiveProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.competitive.programming.competitiveprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitiveProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitiveProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/competitive/programming/competitiveprogramming/CompetitiveProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.competitive.programming.competitiveprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitiveProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
